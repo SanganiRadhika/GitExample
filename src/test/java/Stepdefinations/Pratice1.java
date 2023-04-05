7e6592348d3e68b3968af1a6175c8048cbfdaac3
Radhika <sanganiradhika15@gmail.com>
2023-04-05 14:20:31 +0530

    lovcal branch changes

--- src/test/java/Stepdefinations/Pratice1.java
+++ src/test/java/Stepdefinations/Pratice1.java
@@ -49,6 +49,7 @@ public class Pratice1 {
 		 System.out.println("emailid of the candidate "+emailid);
 		 System.out.println("This is the Git Example from the cloning project");
 		 System.out.println("This is the example for real person done the changes in code");
+		 System.out.println("local branch");
 		 
 	 }
 
